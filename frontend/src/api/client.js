ccc9b9e61eeec3729ba08c607841c3865c53d77f
yuanq10 <loganyuan6@gmail.com>
2026-05-02 03:23:06 -0400

    bug fix

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const API_BASE_URL = "REPLACE_WITH_RAILWAY_URL"; // e.g. https://centsyve-api.railway.app
+const API_BASE_URL = "https://centsyve-production.up.railway.app";
 
 const client = axios.create({ baseURL: API_BASE_URL });
 
